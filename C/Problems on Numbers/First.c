3a5a7f705e7100614610b70cd0cb592d24ef26f3
shindevaibhav434 <89913250+shindevaibhav434@users.noreply.github.com>
2024-12-18 17:42:33 +0530

    Create First.c

---
+++ C/Problems on Numbers/First.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // type of data type in c 
+    /*
+    primitive data types : inbuilt data types : int , char , float , double , void 
+    derived data types  : derived from primitive or inbuilt data types : array , pointer , function 
+    user defind data types : defind by the user himself : structure , union , enum 
+    */
+    
+    int no = 11;
+    printf("Entered number is : %d",no);
+    printf("d",sizeof(no));
+    
+    return 0;
+}
